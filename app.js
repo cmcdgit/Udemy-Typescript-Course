87a36eedc0e8b4a7c2cbf4d727625d59ecd776ea
Ciaran McDevitt <cmcd167@outlook.com>
2020-10-07 17:56:20 +0100

    example 3

---
+++ app.js
@@ -0,0 +1,6 @@
+var person = {
+    name: 'Ciaran',
+    age: 40
+};
+console.log(person.name);
+console.log(person.age);
---
+++ app.ts
@@ -0,0 +1,10 @@
+const person: {
+  name: string,
+  age: number
+} = {                          //{} - typescript notation for OBJECT
+  name: 'Ciaran',
+  age: 40
+};
+
+console.log(person.name);
+console.log(person.age);
