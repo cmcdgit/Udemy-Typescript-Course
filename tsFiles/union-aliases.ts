5f615b4b96add5d2970fe3950a773b54263a7d52
Ciaran McDevitt <cmcd167@outlook.com>
2020-10-08 10:05:28 +0100

    reconfigure project

---
+++ tsFiles/union-aliases.ts
@@ -0,0 +1,33 @@
+type Combinable = number | string;                       //TYPE ALIASES
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(
+  input1: Combinable,
+  input2: Combinable,
+  resultConversion: ConversionDescriptor    //LITERALS - result conversion has to be any of the predefined values
+) {  //example of defining UNIONS
+  // const result = input1 + input2;   //typescript gives out because it can't determine if multiple types allow +
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === "as-number") {
+    result = +input1 + +input2;   //typescript gives out because it can't determine if multiple types allow +  
+  } else {
+    result = input1.toString() + ' ' + input2.toString();
+  }
+
+  return result;
+
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
+}
+
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+
+const combinedStringAges = combine('30', '26', 'as-number');
+console.log(combinedStringAges);
+
+const combinedNames = combine('Ciaran', 'Marie', 'as-text');
+console.log(combinedNames);
